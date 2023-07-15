42fe1d3ff8d907bda4276b91eb23eb0518d60967
Joao Vitor <joaovlopes000@gmail.com>
2023-07-14 22:20:11 -0300

    base structure

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,1 +1,45 @@
-console.log('teste');
+interface UserController {
+    saveContent(request: any, response: any): any;
+    getContent(request: any, response: any): any;
+}
+
+const saveContent = async (request: any, response: any) => {
+    try {
+        const { body } = request;
+        console.log(
+            `[POST] ${JSON.stringify(body)}`
+        )
+
+        if (!body.uuid) return Exception.RequiredFields("uuid", response);
+
+        const result = await contentService.saveContent(body);
+
+        return response.status(201).json(result)
+    } catch (ex) {
+        throw ex
+    }
+}
+
+const getContent = async (request: any, response: any) => {
+    try {
+        const uuid = request.params.uuid;
+        console.log(
+            `[GET] UUID = uuid`
+        )
+
+        if (!uuid) return Exception.RequiredFields("uuid", response);
+
+        const result = await contentService.getContent(uuid);
+
+        return response.status(200).json(result);
+    } catch (ex) {
+        throw ex
+    }
+}
+
+const userController: UserController = {
+    saveContent,
+    getContent
+}
+
+export default userController;
